27a441a9ae0fdf4213a1c4a3ceefdd55984f8eaa
Carlos <trick@192.168.1.15>
2020-08-22 13:25:29 -0300

    Refatoração da tela de cadastro

---
+++ QxdCarRent/src/com/ufc/br/QxdCarRent/boundary/util/CustomComponents/CustomFormattedTextField.java
@@ -0,0 +1,48 @@
+package com.ufc.br.QxdCarRent.boundary.util.CustomComponents;
+
+import java.awt.Graphics;
+
+import javax.swing.Icon;
+import javax.swing.JFormattedTextField;
+import javax.swing.text.MaskFormatter;
+import javax.swing.border.Border;
+
+public class CustomFormattedTextField extends JFormattedTextField{
+	private static final long serialVersionUID = 1L;
+	private CustomTextFieldHelper mHelper = new CustomTextFieldHelper(this);
+
+	public CustomFormattedTextField() {
+		super();
+	}
+
+	public CustomFormattedTextField(MaskFormatter mask) {
+		super(mask);
+	}
+
+	private CustomTextFieldHelper getHelper() {
+		if (mHelper == null)
+			mHelper = new CustomTextFieldHelper(this);
+
+		return mHelper;
+	}
+
+	@Override
+	protected void paintComponent(Graphics graphics) {
+		super.paintComponent(graphics);
+		getHelper().onPaintComponent(graphics);
+	}
+
+	public void setIcon(Icon icon) {
+		getHelper().onSetIcon(icon);
+	}
+
+	public void setIconSpacing(int spacing) {
+		getHelper().onSetIconSpacing(spacing);
+	}
+
+	@Override
+	public void setBorder(Border border) {
+		getHelper().onSetBorder(border);
+		super.setBorder(getHelper().getBorder());
+	}
+}
--- QxdCarRent/src/com/ufc/br/QxdCarRent/boundary/view/SignUp.java
+++ QxdCarRent/src/com/ufc/br/QxdCarRent/boundary/view/SignUp.java
@@ -1,15 +1,20 @@
 package com.ufc.br.QxdCarRent.boundary.view;
 
 import java.awt.EventQueue;
+import java.text.ParseException;
 
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.text.MaskFormatter;
 
+import com.ufc.br.QxdCarRent.boundary.util.CustomComponents.CustomFormattedTextField;
 import com.ufc.br.QxdCarRent.boundary.util.CustomComponents.CustomPasswordField;
 import com.ufc.br.QxdCarRent.boundary.util.CustomComponents.CustomTextField;
 
 import javax.swing.JLabel;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import java.awt.Color;
 import javax.swing.JButton;
@@ -46,7 +51,7 @@ public class SignUp extends JDialog {
 		setResizable(false);
 		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		setModal(true);
-		setBounds(100, 100, 310, 500);
+		setBounds(100, 100, 310, 535);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -59,37 +64,71 @@ public class SignUp extends JDialog {
 		
 		JPanel panelSignUpScreenBackground = new JPanel();
 		panelSignUpScreenBackground.setBackground(new Color(240, 255, 240));
-		panelSignUpScreenBackground.setBounds(0, 242, 304, 229);
+		panelSignUpScreenBackground.setBounds(0, 242, 304, 255);
 		contentPane.add(panelSignUpScreenBackground);
 		panelSignUpScreenBackground.setLayout(null);
 		
 		CustomTextField textFieldSignUpScreenName = new CustomTextField();
-		textFieldSignUpScreenName.setBounds(41, 0, 219, 20);
+		textFieldSignUpScreenName.setBounds(41, 0, 219, 40);
+		textFieldSignUpScreenName.setBackground(new Color(240, 255, 240));
+		textFieldSignUpScreenName.setBorder(
+	            BorderFactory.createTitledBorder(
+	                    BorderFactory.createEtchedBorder(
+	                            EtchedBorder.RAISED, Color.GRAY
+	                            , Color.DARK_GRAY), "Nome Completo"));
 		panelSignUpScreenBackground.add(textFieldSignUpScreenName);
 		textFieldSignUpScreenName.setColumns(10);
 		textFieldSignUpScreenName.setIcon(new ImageIcon(SignUp.class.getResource("/com/ufc/br/QxdCarRent/boundary/assets/icons/name.png")));
 		
-		CustomTextField textFieldSignUpScreenCPF = new CustomTextField();
-		textFieldSignUpScreenCPF.setBounds(41, 42, 219, 20);
+		MaskFormatter cpfMask = null;
+		try {
+			cpfMask = new MaskFormatter("###.###.###-##");
+			cpfMask.setPlaceholderCharacter('_');
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		CustomFormattedTextField textFieldSignUpScreenCPF = new CustomFormattedTextField(cpfMask);
+		textFieldSignUpScreenCPF.setBounds(41, 51, 219, 40);
+		textFieldSignUpScreenCPF.setBackground(new Color(240, 255, 240));
+		textFieldSignUpScreenCPF.setBorder(
+	            BorderFactory.createTitledBorder(
+	                    BorderFactory.createEtchedBorder(
+	                            EtchedBorder.RAISED, Color.GRAY
+	                            , Color.DARK_GRAY), "CPF"));
 		panelSignUpScreenBackground.add(textFieldSignUpScreenCPF);
 		textFieldSignUpScreenCPF.setIcon(new ImageIcon(SignUp.class.getResource("/com/ufc/br/QxdCarRent/boundary/assets/icons/cpf.png")));
 		
 		CustomTextField textFieldSignUpScreenEmail = new CustomTextField();
-		textFieldSignUpScreenEmail.setBounds(41, 84, 219, 20);
+		textFieldSignUpScreenEmail.setBounds(41, 105, 219, 40);
+		textFieldSignUpScreenEmail.setBackground(new Color(240, 255, 240));
+		textFieldSignUpScreenEmail.setBorder(
+	            BorderFactory.createTitledBorder(
+	                    BorderFactory.createEtchedBorder(
+	                            EtchedBorder.RAISED, Color.GRAY
+	                            , Color.DARK_GRAY), "Email"));
 		panelSignUpScreenBackground.add(textFieldSignUpScreenEmail);
 		textFieldSignUpScreenEmail.setIcon(new ImageIcon(SignUp.class.getResource("/com/ufc/br/QxdCarRent/boundary/assets/icons/email.png")));
 		
 		CustomPasswordField passwordFieldSignUpScreenPassword = new CustomPasswordField();
-		passwordFieldSignUpScreenPassword.setBounds(41, 129, 219, 20);
+		passwordFieldSignUpScreenPassword.setBounds(41, 156, 219, 40);
+		passwordFieldSignUpScreenPassword.setBackground(new Color(240, 255, 240));
+		passwordFieldSignUpScreenPassword.setBorder(
+	            BorderFactory.createTitledBorder(
+	                    BorderFactory.createEtchedBorder(
+	                            EtchedBorder.RAISED, Color.GRAY
+	                            , Color.DARK_GRAY), "Senha"));
 		passwordFieldSignUpScreenPassword.setIcon(new ImageIcon(SignUp.class.getResource("/com/ufc/br/QxdCarRent/boundary/assets/icons/password.png")));
 		panelSignUpScreenBackground.add(passwordFieldSignUpScreenPassword);
 		
 		JButton buttonSignUpScreenFinish = new JButton("Concluir");
-		buttonSignUpScreenFinish.setBounds(171, 173, 89, 23);
+		buttonSignUpScreenFinish.setBounds(171, 215, 89, 23);
+		buttonSignUpScreenFinish.setBackground(new Color(255, 200, 0));
 		panelSignUpScreenBackground.add(buttonSignUpScreenFinish);
 		
 		JButton buttonSignUpScreenCancel = new JButton("Cancelar");
-		buttonSignUpScreenCancel.setBounds(41, 173, 89, 23);
+		buttonSignUpScreenCancel.setBounds(41, 215, 89, 23);
+		buttonSignUpScreenCancel.setBackground(new Color(255, 200, 0));
 		panelSignUpScreenBackground.add(buttonSignUpScreenCancel);
 	}
 }
